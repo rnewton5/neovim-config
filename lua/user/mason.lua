8beaa3951d38787052287f223f877971d851e995
rhett <rhett.newton@gmail.com>
2022-09-04 15:03:10 -0500

    Add Mason

---
+++ lua/user/mason.lua
@@ -0,0 +1,16 @@
+local function setup_plugin(plugin_name, opts)
+  local plugin_ok, plugin = pcall(require, plugin_name)
+  if not plugin_ok then
+    vim.notify(plugin_name .. " not found")
+    return false
+  else 
+    plugin.setup(opts)
+    return true
+  end
+end
+
+local mason_ok = setup_plugin("mason", {})
+if mason_ok then
+  setup_plugin("mason-lspconfig", {})
+end
+
